e5ad5611bedea228108bf1ab71846cc2e6ce1489
haiqaldani <muhammadhaiqaldani@gmail.com>
2025-07-06 19:14:57 +0700

    wrong comman build

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -10,10 +10,9 @@ echo -e "Login ke GitHub Container Registry"
 docker login ghcr.io -u "$USERNAME_GITHUB" -p $PAT_GITHUB
 
 # Build image Docker backend
-
 echo -e "Build Image"
-docker build -t ghcr.io/$USERNAME_GITHUB/$IMAGE_NAME:$IMAGE_VERSION.
+docker build -t ghcr.io/$USERNAME_GITHUB/$IMAGE_NAME:$IMAGE_VERSION .
 
 # Push image ke GitHub Container Registry
 echo -e "Push Image ke Github"
-docker push ghcr.io/$USERNAME_GITHUB/$IMAGE_NAME:$IMAGE_VERSION.
+docker push ghcr.io/$USERNAME_GITHUB/$IMAGE_NAME:$IMAGE_VERSION
