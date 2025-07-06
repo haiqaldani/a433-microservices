9d1d1834957327bc6f99e2c252f0f31caefaeb43
haiqaldani <muhammadhaiqaldani@gmail.com>
2025-07-06 14:20:11 +0700

    change build

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+USERNAME_GITHUB="haiqaldani"
+IMAGE_NAME="item-app"
+IMAGE_VERSION="v1"
+
 
 # Membangun Docker image dari Dockerfile di direktori saat ini, dengan tag v1
-docker build -t item-app:v1 .
+echo -e "Build a new Docker image:"
+docker build -t $IMAGE_NAME:$IMAGE_VERSION .
 
-# Login ke GitHub Container Registry menggunakan Personal Access Token (PAT)
-echo $PAT_GITHUB | docker login ghcr.io -u haiqaldani--password-stdin
+#List Docker Images
+echo -e "List of Docker images:"
+docker images
 
 # Memberi tag image sesuai format GitHub Container Registry
-docker tag item-app:v1 ghcr.io/haiqaldani/item-app:v1
+
+echo -e "Giving a tag"
+docker tag $IMAGE_NAME:$IMAGE_VERSION ghcr.io/$USERNAME_GITHUB/$IMAGE_NAME:$IMAGE_VERSION
+docker images
+
+# Login ke GitHub Container Registry menggunakan Personal Access Token (PAT)
+echo -e "Login into Github"
+echo $PAT_GITHUB | docker login ghcr.io -u $USERNAME_GITHUB --password-stdin
 
 # Push image ke GitHub Container Registry
-docker push ghcr.io/haiqaldani/item-app:v1
+echo -e "Publishing Image to Github"
+docker push ghcr.io/$USERNAME_GITHUB/$IMAGE_NAME:$IMAGE_VERSION
