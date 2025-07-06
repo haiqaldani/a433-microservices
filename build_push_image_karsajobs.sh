06707bf1112b037001938514578dffd02972b07d
haiqaldani <muhammadhaiqaldani@gmail.com>
2025-07-06 17:32:29 +0700

    push config and deployment

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Pengaturan user dan repository untuk GitHub Container Registry
+USERNAME_GITHUB="haiqaldani"
+IMAGE_NAME="karsajobs"
+IMAGE_VERSION="lastest"
+
+# Login ke GitHub Container Registry (GitHub Packages)
+echo -e "Login ke GitHub Container Registry"
+echo "$PAT_GITHUB" | docker login ghcr.io -u "$USERNAME_GITHUB" --password-stdin
+
+# Build image Docker backend
+
+echo -e "Build Image"
+docker build -t ghcr.io/$USERNAME_GITHUB/$IMAGE_NAME:$IMAGE_VERSION.
+
+# Push image ke GitHub Container Registry
+echo -e "Push Image ke Github"
+docker push ghcr.io/$USERNAME_GITHUB/$IMAGE_NAME:$IMAGE_VERSION.
