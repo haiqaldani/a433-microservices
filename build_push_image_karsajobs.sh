619523065afd6d9fc51cbd22c97cf10d1ec4b06b
haiqaldani <muhammadhaiqaldani@gmail.com>
2025-07-06 18:55:30 +0700

    change docker login

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -7,10 +7,9 @@ IMAGE_VERSION="lastest"
 
 # Login ke GitHub Container Registry (GitHub Packages)
 echo -e "Login ke GitHub Container Registry"
-echo "$PAT_GITHUB" | docker login ghcr.io -u "$USERNAME_GITHUB" --password-stdin
+docker login ghcr.io -u "$USERNAME_GITHUB" -p $PAT_GITHUB
 
 # Build image Docker backend
-
 echo -e "Build Image"
 docker build -t ghcr.io/$USERNAME_GITHUB/$IMAGE_NAME:$IMAGE_VERSION.
 
